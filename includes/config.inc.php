2d94d6e50f025bd1b2e3c16ae21e56a26932db6b
LiXCode <lixcode@teamast.io>
2019-04-08 11:43:12 +0900

    Rollup 2019-04-08

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -1,14 +1,13 @@
 <?php
 date_default_timezone_set("Asia/Seoul");
 
-$version = '0.4'; // version tag
+$version = '0.4.1'; // version tag
 $debug = true; // debug mode
 
-if(!defined("__VCPLOADED__")) {
+if(!defined("__EFLIX_LOADED__")) {
 	if(!defined("__VERSION__")) define("__VERSION__", $version);
-	if(!defined("__VCP__")) define("__VCP__", true);
 	if(!defined("__EFLIX__")) define("__EFLIX__", true);
-	if(!defined("_VCPDEBUG_")) define("_VCPDEBUG_", $debug);
+	if(!defined("_EFLIXDEBUG_")) define("_EFLIXDEBUG_", $debug);
 	if(error_reporting() !== E_ALL & ~E_NOTICE) {
 		ini_set('display_errors', true);
 		error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);
@@ -31,30 +30,24 @@ if(!defined("__VCPLOADED__")) {
 	if($vcpPath != '/') $vcpRoot .= $vcpPath . '/';
 	
 	// _VCPROOT_ 절대경로
-	if(!defined("_VCPROOT_")) {
-		define("_VCPROOT_", $vcpRoot);
-	}
+	if(!defined("_EFLIXROOT_")) define("_EFLIXROOT_", $vcpRoot); 
 	// _VCPPATH_ 상대경로
-	if(!defined("_VCPPATH_")) {
-		define("_VCPPATH_", $vcpPath);
-	}
-	if(!defined("_VCPHOST_")) {
-		define("_VCPHOST_", $vcpHost);
-	}
-
-	if(!defined("_VIRTHOST_")) {
-		define("_VIRTHOST_", _VCPHOST_);
-	}
-	if(!defined("_VIRTPATH_")) {
-		define("_VIRTPATH_", _VCPPATH_);
-	}
-	if(!defined("_VIRTROOT_")) {
-		define("_VIRTROOT_", _VCPROOT_);
-	}
+	if(!defined("_EFLIXPATH_")) define("_EFLIXPATH_", $vcpPath); 
+	if(!defined("_EFLIXHOST_")) define("_EFLIXHOST_", $vcpHost); 
+	
+	// 호환성 변수
+	if(!defined("__VCP__")) define("__VCP__", true);
+	if(!defined("_VCPDEBUG_")) define("_VCPDEBUG_", _EFLIXDEBUG_);
+	if(!defined("_VCPROOT_")) define("_VCPROOT_", _EFLIXROOT_);
+	if(!defined("_VIRTROOT_")) define("_VIRTROOT_", _VCPROOT_);
+	if(!defined("_VCPPATH_")) define("_VCPPATH_", _EFLIXPATH_);
+	if(!defined("_VIRTPATH_")) define("_VIRTPATH_", _VCPPATH_);
+	if(!defined("_VCPHOST_")) define("_VCPHOST_", _EFLIXHOST_);
+	if(!defined("_VIRTHOST_")) define("_VIRTHOST_", _VCPHOST_);
 	
 	include_once(_VIRTROOT_ . "/includes/func.inc.php");
 	include_once(_VIRTROOT_ . "/includes/route.inc.php");
 	
-	define("__VCPLOADED__", true);
+	define("__EFLIX_LOADED__", true);
 
 }
